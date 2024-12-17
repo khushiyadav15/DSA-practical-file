2779f54fe301af676f15e54ebe503f3c4048a89a
khushiyadav15 <0902cs231050@rjit.ac.in>
2024-12-17 14:11:26 +0530

    Add files via upload

--- section E/exp1c.c
+++ section E/exp1c.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
-// Function to implement Bubble Sort
-void bubbleSort(int arr[], int n)
+// Function to implement Insertion Sort
+void insertionSort(int arr[], int n)
 {
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 1; i < n; i++)
     {
-        // Last i elements are already sorted
-        for (int j = 0; j < n - i - 1; j++)
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements of arr[0..i-1] that are greater than key
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > key)
         {
-            if (arr[j] > arr[j + 1])
-            {
-                // Swap arr[j] and arr[j+1]
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
+            arr[j + 1] = arr[j];
+            j--;
         }
+
+        arr[j + 1] = key;
     }
 }
 
@@ -46,15 +47,14 @@ int main()
         scanf("%d", &arr[i]);
     }
 
-    // Perform Bubble Sort
+    // Perform Insertion Sort
     printf("\nUnsorted array: ");
     printArray(arr, n);
 
-    bubbleSort(arr, n);
+    insertionSort(arr, n);
 
     // Print sorted array
     printf("Sorted array: ");
     printArray(arr, n);
 
-    return 0; // Explicit return for program termination
-}
+    return 0; // Explicit return for program termination} 
